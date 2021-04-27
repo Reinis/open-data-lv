f240164ccb6a7a7f1ccefe360c665e576d43fda5
Reinis Danne <rei4dan@gmail.com>
2021-04-27 14:23:22 +0300

    composer: Add Laravel Telescope
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
         // Laravel IDE helper
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
